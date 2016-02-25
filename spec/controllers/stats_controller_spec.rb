aa28e4a93c50c28e4175abb7ba16527d5e943e36
Brett <brettrheenan@gmail.com>
2016-02-25 16:10:37 +0100

    Done.

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe StatsController, type: :controller do
+RSpec.describe Admin::StatsController, type: :controller do
+
+	it 'returns a 200 status' do
+		visit admin_stats_path
+		expect(response.status).to eq(200)
+	end
 
 end
