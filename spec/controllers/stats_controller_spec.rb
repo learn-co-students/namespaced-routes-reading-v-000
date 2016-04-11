793180bba968383cb3a6fc44653176705d5cca31
Brian Holland <brian.holland@proatria.com>
2016-04-11 08:42:25 -0500

    Done.

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe StatsController, type: :controller do
+# RSpec.describe StatsController, type: :controller do
+RSpec.describe Admin::StatsController, type: :controller do
 
 end
