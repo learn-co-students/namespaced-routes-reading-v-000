29a9b91eeefbdf4d66495ea2d176293b3c6ff5ef
glee38 <glee3388@gmail.com>
2016-05-03 17:00:26 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,18 @@ Rails.application.routes.draw do
     resources :posts, only: [:show, :index, :new, :edit]
   end
 
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
+
 end
--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -1,5 +1,2 @@
 require 'rails_helper'
 
-RSpec.describe StatsController, type: :controller do
-
-end
