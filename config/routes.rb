7ee8bb10877515e4126e75efd4be135110bf2f26
seanliu93 <sean93liu@gmail.com>
2016-05-06 23:47:30 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'stats/index'
+
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
   end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
+  get '/admin/stats', to: 'stats#index'
   root 'posts#index'
 end
