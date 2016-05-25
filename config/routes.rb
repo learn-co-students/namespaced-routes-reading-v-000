edd07f9107d3f67aa101e98b97b78fc9d9b98539
hudbuz <hzb2@cornell.edu>
2016-05-25 14:56:46 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
   end
 
+ 
+
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+ 
 
   root 'posts#index'
 end
