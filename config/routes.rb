07c4cec161d48aec951fedf0bcc8c21a25530f76
Bryan Hou <bryan.hou94@gmail.com>
2017-06-05 11:37:16 +0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  #scope prefixes the routes. eg. /admin/stats/index.html.erb
+  #module indicates that the controller is nested under folder admin
+
+
+  #scope '/admin', module: 'admin' do 
+  #stats_path
+  namespace :admin do
+  #rouch changes to admin_stats_path
+	  resources :stats, only: [:index]
+	end
 end
