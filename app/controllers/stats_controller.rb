9c3ab6a49ea8c30c016a96b19d0d370a71fa1eae
github username <github email address>
2017-11-01 15:56:32 -0700

    Done.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,10 @@
 class StatsController < ApplicationController
 
   def index
+    require 'pry'
     @post_count = Post.count
     @authors_count = Author.count
     @last_post = Post.last
+    binding.pry
   end
 end
