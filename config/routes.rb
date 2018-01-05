16891c87b1b38c0d1a5a17d321701b6470f25b0d
github username <github email address>
2018-01-05 14:51:20 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :admin do # the URL path prefix "/admin" matches the name of the Admin module
+    resources :stats, only: [:index]
+  end
+  # Above code is Rails shortcut for saying:
+  #scope '/admin', module: 'admin' do
+    #resources :stats, only: [:index]
+  #end
+
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
   end
