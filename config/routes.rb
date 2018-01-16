c20984c0db7c2ef2cf8627429b0944e25ee5c48f
Jessie Huff <huffjd1@gmail.com>
2018-01-16 15:51:03 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+  namespace :admin do #the namespace method makes the assumption that the path prefix and module name match
+    resources :stats, only: [:index] #using scope gave us a stats_path helper, namespace helper is prefixed with admin_ so stats_path becomes admin_stats_path
+  end
 end
