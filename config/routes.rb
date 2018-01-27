999f05893d9e1d057b9dc0759fedd3fc8c1da06d
kpeck71 <kpeck71@gmail.com>
2018-01-27 18:42:33 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  #scope '/admin', module: 'admin' do
+  #When we want to route with a module and use that module's name as the URL prefix,
+  #we can use the namespace method instead of scope, module
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
 end
