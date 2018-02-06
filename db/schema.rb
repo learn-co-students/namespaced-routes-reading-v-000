8e9a1a78d6ca15b2e0e6acaf65a4e5baff7106a9
Donnadieu <donnadieu.86@gmail.com>
2018-02-05 19:01:38 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113011820) do
+ActiveRecord::Schema.define(version: 20180205235104) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20160113011820) do
     t.datetime "updated_at",  null: false
     t.boolean  "post_status"
     t.integer  "author_id"
+  end
+
+  create_table "stats", force: :cascade do |t|
+    t.string   "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
