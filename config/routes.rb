2b61e3e5e52aef9083003aa7bb074000276d61e8
github username <github email address>
2018-02-21 16:38:38 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+# This...
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+  # is the same as this:
+      # scope '/admin', module: 'admin' do
+      #   resources :stats, only: [:index]
+      # end
+  # only more D.R.Y.
+  # If you do this update any related URL helpers!
 end
