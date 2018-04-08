e00f9688a0345d050a2c1b9d014d91b97fa01dd2
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-08 07:22:41 -0400

    Learn about admin modules, namespacing routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  namespace :admin do
+    resources :stats, only: [:index]
+    # resources :authors, only: [:new, :create, :delete]
+    # resources :comments, only: [:moderate]
+  end
+
+  # get '/admin/stats' to: 'stats#index'
+  get '/admin/authors/new', to: 'authors#new'
+  get '/admin/authors/delete', to: 'authors#delete'
+  get '/admin/authors/create', to: 'authors#create'
+  get '/admin/comments/moderate', to: 'comments#moderate'
+
   root 'posts#index'
 end
