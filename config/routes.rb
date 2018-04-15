889a17b097d7e3c9a516992d1ac23fa65931dfed
Penguino <heriberto@melo.nyc>
2018-04-15 14:06:47 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
     resources :posts, only: [:show, :index, :new, :edit]
   end
 
+  #get '/admin/stats', to: 'stats#index'
+  #scope '/admin', module: 'admin' do
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
