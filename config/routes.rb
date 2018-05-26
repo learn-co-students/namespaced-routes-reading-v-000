a7e4773849a479145e5611a7053863342f35d247
github username <github email address>
2018-05-26 12:55:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    # Scoping Routes
+    #   Scope lets us hold a lot of routes under one grouping
+    # Scoping with Modules
+    #   We’ll make a controller to hold all of the admin features
+    #   Updates views to reflect new admin controller
+    #   Update routes to call module in admin controller
+    # Namespace
+    #   Changes URL helpers
+    #   You’ll need to update helpers files to reflect this 
     resources :stats, only: [:index]
   end
 
