91e6a212711a6c92b9caaa122338f7592b71b04c
AlexFriedland <friedlandaj@gmail.com>
2018-06-11 20:36:13 +0200

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,22 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  #turn this:
+  #get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
+
+  #into namespaced this:
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  #refactor into NAMESPACE
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   root 'posts#index'
 end
