6dbac38812dea25d5e20f345961d3fc17cef34c5
Gingertonic <bethmschofield@gmail.com>
2018-06-24 17:42:15 +0930

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,16 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+#gives stats_path & stats_url
+  # get '/admin/stats', to: 'stats#index'
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+#gives admin_stats_path & admin_stats_url
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   root 'posts#index'
 end
