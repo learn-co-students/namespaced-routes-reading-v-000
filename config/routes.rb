9d32f15b024859f0b479001db097ad37c8bfc54e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-31 23:27:36 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  # equivalent to this is below: get '/admin/stats', to: 'stats#index'
+  # but with scope, you can nicely add additional routes
+  # 'module' needed because using namespace (subfolders)
+  #  scope '/admin', module: 'admin'do   ==> helper stats_path
+  # namespace keyword is sort of shorthand for the scope/module  ==> helper admin_stats_path
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   root 'posts#index'
 end
