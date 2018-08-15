1aeac6a4f1e7199bc4629e145cb9c0ddff85ca73
Aly Alidina <aly_alidina@hotmail.com>
2018-08-15 10:53:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-
+  # get '/admin/stats', to: 'stats#index' route admin/stats to #diplay erb index.
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+  #We can use namespace instead of scope and it assumes that the #path prefix and module name match
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
   end
