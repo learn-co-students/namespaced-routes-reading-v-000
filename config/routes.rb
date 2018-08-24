21f1a75336969094eacd869e7e78da1f48254375
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-24 21:43:08 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+
+
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'atuhors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   root 'posts#index'
 end
