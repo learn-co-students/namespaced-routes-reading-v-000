422c9506500dbe29d0ae73d22f32a4662e317476
emanuelbierman <emanuel.bierman@gmail.com>
2018-10-30 15:15:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,21 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # scope '/admin' , module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+  # We're telling `scope` that we want to use `/admin` as a URL prefix, and we're
+  # also letting Rails know that all of the included routes will be handled by
+  # controllers in the `admin` module.
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+  # **Top-tip:** There is one important difference between `scope '/admin', module: 'admin'` and
+  # `namespace :admin`, and it's in the URL helpers. Remember above that using
+  # `scope` gave us a `stats_path` helper. But now that we are using `namespace`,
+  # run `rake routes` again. You'll see that the helper is now prefixed with
+  # `admin_`, so `stats_path` becomes `admin_stats_path`. If you switch from `scope`
+  # to `namespace`, take care to update any URL helpers you have in use!
 
   root 'posts#index'
 end
