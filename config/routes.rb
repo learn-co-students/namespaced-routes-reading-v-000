5cbead2d5599c7ef12822f0d0fcefa97645decf6
arye-dov-eidelman <aryedov.e@gmail.com>
2018-11-04 13:28:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :authors, only: %i[show index] do
-    resources :posts, only: %i[show index new edit]
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
   end
 
-  resources :posts, only: %i[index show new create edit update]
+  resources :posts, except: [:delete, :destroy]
 
-  get '/admin/stats', to: 'stats#index'
+  namespace :admin do
+    resources :stats, only: :index
+  end
 
   root 'posts#index'
 end
