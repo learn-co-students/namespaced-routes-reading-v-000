61c20d1e43280cf9b98b6c5dd3a86b8b5eafaf79
Pamela Frieze <pfrieze@gmail.com>
2018-11-05 22:09:05 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  
+  get '/stats', to: 'stats#index'
+  
   resources :authors, only: %i[show index] do
     resources :posts, only: %i[show index new edit]
   end
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
-
+  namespace :admin do
+  resources :stats, only: [:index]
+end
+  
   root 'posts#index'
 end
