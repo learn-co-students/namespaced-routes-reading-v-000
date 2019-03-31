3610d436c5593ae6b55a31154d49e14cb3fe879d
kmead <kimmeadows8@gmail.com>
2019-03-30 21:20:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :authors, only: %i[show index] do
     resources :posts, only: %i[show index new edit]
+    namespace :admin do 
+      resources :stats, only: [:index]
+    end 
   end
 
   resources :posts, only: %i[index show new create edit update]
