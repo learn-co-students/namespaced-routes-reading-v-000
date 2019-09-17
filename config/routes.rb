8f650cf8214be5203eb9dc50562cc744f783204c
github username <github email address>
2019-09-17 12:12:07 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,14 @@ Rails.application.routes.draw do
   #Fortunately, Rails gives us a shortcut here. 
   #When we want to route with a module and use that module's name as the URL prefix, 
   #we can use the namespace method instead of scope, module.
+=begin
   scope '/admin', module: 'admin' do
     resources :stats, only: [:index]
   end
+=end
 
+namespace :admin do
+  resources :stats, only: [:index]
+end
 
 end
