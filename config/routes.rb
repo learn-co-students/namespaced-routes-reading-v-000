846f269f0e7e8bf46c85309f8c2c2c939e87c14a
mat_anderson <matandersonlv@hotmail.com>
2019-10-24 10:05:28 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,17 @@ Rails.application.routes.draw do
     resources :posts, only: %i[show index new edit]
   end
 
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  get '/stats', to: 'stats#index'
+  get '/authors/new', to: 'authors#new'
+  get '/authors/delete', to: 'authors#delete'
+  get '/authors/create', to: 'authors#create'
+  get '/comments/moderate', to: 'comments#moderate'
 
   root 'posts#index'
 end
