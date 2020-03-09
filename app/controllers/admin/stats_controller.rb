e97bfc7c8c9fdd295a83d81414dcfe78622c1662
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-03-09 20:48:34 +0000

    Done.

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:app/controllers/admin/stats_controller.rb
 class Admin::StatsController < ApplicationController
-=======
-class Admin::  StatsController < ApplicationController
->>>>>>> 5185520c521c975936111c14e6d706f87517117c:app/controllers/admin/stats_controller.rb
-
   def index
     @post_count = Post.count
     @authors_count = Author.count
